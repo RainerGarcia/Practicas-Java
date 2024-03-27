9341fe0b2e042978f4bedbfc466a70dcfdcc97c0
RainerGarcia <122415665+RainerGarcia@users.noreply.github.com>
2024-03-27 16:11:48 -0400

    Update mercury_tours_logintest.java

--- proyectos-Java/automatizacion-selenium/primeros_test/mercury_tours_logintest.java
+++ proyectos-Java/automatizacion-selenium/primeros_test/mercury_tours_logintest.java
@@ -1,28 +1,59 @@
-package selenium.maven.practicas.primeros_test;
+package selenium.maven.practicas.prueba;
 
 import org.testng.annotations.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.By;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class mercury_tours_logintest {
 
         EdgeDriver driver = new EdgeDriver();
+        String usuario = "caso@email.com";
+        String contraseña = "12345";
         
-        @BeforeAll public void inicio() throws InterruptedException{
+        @BeforeSuite public void inicio() throws InterruptedException{
 
-
-        }
-                
-        @Test public void prueba(){
-
-
+                driver.get("https://demo.guru99.com/test/newtours/");
+                Thread.sleep(10000);
 
         }
                 
+        @Test(priority = 1) public void registro() throws InterruptedException{
 
-        @AfterAll public void finalizar(){
-                driver.quit();
+                driver.findElement(By.linkText("REGISTER")).click();
+                Thread.sleep(10000);
+                driver.findElement(By.id("email")).sendKeys(usuario);
+                driver.findElement(By.name("password")).sendKeys(contraseña);
+                driver.findElement(By.name("confirmPassword")).sendKeys(contraseña);
+                driver.findElement(By.name("submit")).click();
+
+
+
+        }
+
+        @Test(priority = 2) public void iniciarsesion() throws InterruptedException{
+
+                Thread.sleep(10000);
+                if(driver.findElement(By.linkText("sign-in")).isDisplayed()){
+
+                        driver.findElement(By.linkText("sign-in")).click();
+                        Thread.sleep(10000);
+                        driver.findElement(By.name("userName")).sendKeys(usuario);
+                        driver.findElement(By.name("password")).sendKeys(contraseña);
+                        driver.findElement(By.name("submit")).click();
+                        Thread.sleep(10000);
+                        driver.findElement(By.xpath("//h3[contains(text(), 'Login Successfully')]")).isDisplayed();
+
+
+                }else{
+                        System.out.println("register no successfully");
+                }
+
+        }
+
+        @AfterSuite public void finalizar(){
+                System.out.println("registro exitoso");
         }
 
 }
